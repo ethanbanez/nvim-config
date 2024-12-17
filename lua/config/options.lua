a41a2d29d4a9994e628c3f97268ed43c512a7b1e
Ethan Banez <ethan.banez@icloud.com>
2024-12-17 12:31:37 -0800

    vim options fixed for better indenting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,10 +4,11 @@
 --
 
 vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-
 vim.opt.expandtab = true
 
 vim.filetype.add({
